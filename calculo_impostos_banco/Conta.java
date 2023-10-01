42090b91fee3f84fbef0b98de283899d6ce5c18f
Rocha <lucasrocha216.lr@gmail.com>
2023-09-30 21:53:05 -0300

    exercicio calcula bancario

---
+++ calculo_impostos_banco/Conta.java
@@ -0,0 +1,23 @@
+class Conta {
+    private double saldo;
+
+    public Conta(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public double obterSaldo() {
+        return saldo;
+    }
+}
